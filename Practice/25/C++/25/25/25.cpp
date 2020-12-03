1931d345cbf69ae98d0ec9e64aebee9208a5e91c
Karina <emelyanovich.karina@mail.ru>
2020-12-03 15:01:35 +0300

    25

---
+++ Practice/25/C++/25/25/25.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <clocale>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+enum Order { up, down };
+
+bool check( vector < int > & x , Order order)
+{
+	switch (order)
+	{
+	case up:
+		for (int i = 0; i < x.size() - 1; i++)
+		{
+			if (x[i] > x[i + 1]) {
+				return false;
+				break;
+			}
+		}
+		break;
+	case down:
+		for (int i = 0; i < x.size() - 1; i++) 
+		{
+			if (x[i] < x[i + 1]) 
+			{
+				return false;
+				break;
+			}
+		}
+		break;
+	}
+
+	return true;
+}
